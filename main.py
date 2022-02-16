f3431e1a21dcac2b016be09031dfe5b25e19c55e
Rohan Karan <rohankaran001@gmail.com>
2022-02-16 11:38:55 +0530

    init

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import json
+import time
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
 
 from recommendations import latest, getRecommendations
 
@@ -7,7 +10,7 @@ app = FastAPI()
 
 origins = [
     "*",
-           ]
+]
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
@@ -17,6 +20,11 @@ app.add_middleware(
 )
 
 
+class InputMovies(BaseModel):
+    id: str
+    tconst: str
+
+
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
@@ -24,17 +32,19 @@ async def root():
 
 @app.get("/movies")
 async def get_movies():
+    start = time.time()
     result = latest()
+    print(time.time() - start)
     if result:
-        return result
+        return json.dumps(result)
     else:
         raise HTTPException(404, "API error! Database is updating. Please try after some time..")
 
 
 @app.post("/get-recommendations")
-async def get_recommendations(movie_name: str):
-    result = getRecommendations(movie_name)
+async def get_recommendations(movie_schema: InputMovies):
+    result = getRecommendations(dict(movie_schema))
     if result:
-        return result
+        return json.dumps(result)
     else:
         raise HTTPException(404, "Data not found!")
--- recommendations.py
+++ recommendations.py
@@ -1,4 +1,4 @@
-import datetime
+from pickle import load
 import time
 from io import BytesIO
 from urllib.error import HTTPError
@@ -11,54 +11,24 @@ from cachetools import cached, TTLCache
 from kaggle import KaggleApi
 from pandas import read_csv
 
-
-def getRecommendations(movie_name):
-    start = time.time()
-
-    def allowSelfSignedHttps(allowed):
-        # bypass the server certificate verification on client side
-        if allowed and not os.environ.get('PYTHONHTTPSVERIFY', '') and getattr(ssl, '_create_unverified_context', None):
-            ssl._create_default_https_context = ssl._create_unverified_context
-
-    allowSelfSignedHttps(True)  # this line is needed if you use self-signed certificate in your scoring service.
-
-    # Request data goes here
-    data = {
-        "Inputs": {
-            "input1":
-                [
-                    {
-                        "Col1": movie_name
-                    },
-                ]
-        },
-        "GlobalParameters": {
-        }
-    }
-
-    body = str.encode(json.dumps(data))
-
-    url = 'http://2b9e4c43-ad5c-491e-8c0a-b81746d0a5d2.centralus.azurecontainer.io/score'
-    api_key = '7oZYovajzxwFr4mTiZgmm21S1nYsTXYp'  # Replace this with the API key for the web service
-    headers = {'Content-Type': 'application/json', 'Authorization': ('Bearer ' + api_key)}
-
-    req = Request(url, body, headers)
-
-    try:
-        response = urlopen(req)
-
-        result = response.read()
-        result = json.loads(result)
-        print(time.time() - start)
-        return result['Results']['output1']
-    except HTTPError as error:
-        print("The request failed with status code: " + str(error.code))
-
-        # Print the headers - they include the requert ID and the timestamp, which are useful for debugging the failure
-        print(error.info())
-
-
 cache = TTLCache(maxsize=1, ttl=86400)
+
+
+@cached(cache)
+def getDataFromKaggle():
+    api = KaggleApi()
+    api.authenticate()
+    link = api.kernel_output(user_name='rohankaran', kernel_slug='mrs-csv')
+    response = get(link['files'][0]['url'])
+    content = response.content
+    result = load(BytesIO(content))
+    return result
+
+
+def getRecommendations(movie_schema):
+    recommendations_list = getDataFromKaggle()
+    if recommendations_list[int(movie_schema["id"])][1] == movie_schema["tconst"]:
+        return recommendations_list[int(movie_schema["id"])][2]
 
 
 @cached(cache)
@@ -66,19 +36,7 @@ def latest():
     api = KaggleApi()
     api.authenticate()
     link = api.kernel_output(user_name='rohankaran', kernel_slug='mrs-csv')
-    response = get(link['files'][0]['url'])
+    response = get(link['files'][1]['url'])
     content = response.content
-    f = read_csv(BytesIO(content))
-
-
-    allm = f['primaryTitle'].tolist()
-
-    trending_movies = f[(f.startYear >= datetime.date.today().year - 1) & (f.titleType == "['movie']")]
-    trending_movies = trending_movies.sort_values(['popularity'], ascending=False).head(10).tconst.tolist()
-    trending_series = f[(f.startYear >= datetime.date.today().year - 1) & (f.titleType == "['tvSeries']")]
-    trending_series = trending_series.sort_values(['popularity'], ascending=False).head(10).tconst.tolist()
-
-    top_movies = f.sort_values(['popularity'], ascending=False).head(10).tconst.tolist()
-    top_series = f.sort_values(['popularity'], ascending=False).head(10).tconst.tolist()
-
-    return {"all": allm, "trm": trending_movies, "trs": trending_series, "tm": top_movies, "ts": top_series}
+    result = load(BytesIO(content))
+    return result
